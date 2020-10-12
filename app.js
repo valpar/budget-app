d0dc253c552256ef2f7efb93e26417d78142dac1
valmar parts <valmarparts@gmail.com>
2020-10-12 18:04:21 +0300

    add event listeners

--- app.js
+++ app.js
@@ -10,18 +10,51 @@ var budgetController = (function() {
 // UI controller
 var  UIController = (function(){
 
-    //some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //wilol be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    }
 
 })();
 
 //gobal app controller
 var controller = (function(budgetCtrl, UICtrl){
 
-    document.querySelector('.add__btn').addEventListener('click',function(){
+    var setupEventListeners = function(){
 
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event){
+            if (event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function (){
         //to do list
 
         //1. get the filed input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         //2. add the item to the budget controller
 
@@ -31,10 +64,13 @@ var controller = (function(budgetCtrl, UICtrl){
 
         // 5. display the budget on the UI
 
-    });
+    };
 
-    document.addEventListener('keypress', function(){
-        
-    })
+    return {
+        init: function(){
+            console.log('application has started');
+            setupEventListeners();
+        }
+    }
 
-})(budgetControllers, UIController);
+})(budgetController, UIController);
